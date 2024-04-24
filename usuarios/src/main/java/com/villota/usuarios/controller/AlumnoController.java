f4334e6c092c6c92f6164d29b98f64760405d9fe
Nicolas <nicolas@dlpbasculas.com>
2024-04-23 19:46:14 -0500

    Add balanceadorTest endpoint to AlumnoController

--- usuarios/src/main/java/com/villota/usuarios/controller/AlumnoController.java
+++ usuarios/src/main/java/com/villota/usuarios/controller/AlumnoController.java
@@ -1,7 +1,10 @@
 package com.villota.usuarios.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +21,18 @@ import com.villota.usuarios.service.AlumnoService;
 public class AlumnoController {
   @Autowired
   AlumnoService alumnoService;
+
+  @Value("${config.balanceador.test}")
+  private String balanceadorTest;
+
+  @GetMapping("/balanceadorTest")
+  public ResponseEntity<?> balanceadorTest(){
+    Map<String, Object> response = new HashMap<String, Object>();
+    response.put("balanceador", balanceadorTest);
+    response.put("alumnos", alumnoService.findAll());
+
+    return ResponseEntity.ok(response);
+  }
 
   @GetMapping("/listarAlumnos")
   public ResponseEntity<?> listarAlumnos(){
